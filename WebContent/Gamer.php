4fb57b7f46a7e95126a28a8f823f29dd6ec4c464
Alexander Seibel <a.seibel90@gmx.de>
2017-11-13 15:20:57 +0100

    added gamer year ranking, new categories

--- WebContent/Gamer.php
+++ WebContent/Gamer.php
@@ -3,16 +3,16 @@
 include('db_connection.php');
 
 if($_GET['year'] == "0000"){
-    $query = "SELECT gamers.*, gamer_year_map.* FROM gamers as gamers, gamer_year_map as gamer_year_map WHERE gamer_year_map.gamer_id = gamers.gamer_id group by gamer_year_map.gamer_id order by gamers.gamer_id asc";
+    $query = "SELECT gamers.*, gamer_year_map.*, gamer_year_ranking.* FROM gamers as gamers, gamer_year_map as gamer_year_map LEFT OUTER JOIN gamer_year_ranking as gamer_year_ranking on gamer_year_ranking.gamer_id = gamer_year_map.gamer_id  WHERE gamer_year_map.gamer_id = gamers.gamer_id group by gamer_year_map.gamer_id order by gamers.gamer_id asc";
 } else {
-    $query = "SELECT gamers.*, gamer_year_map.* FROM gamers as gamers, gamer_year_map as gamer_year_map WHERE gamer_year_map.gamer_id = gamers.gamer_id and gamer_year_map.year = ".$_GET['year']." order by gamer_number asc";
+    $query = "SELECT gamers.*, gamer_year_map.*, gamer_year_ranking.* FROM gamers as gamers, gamer_year_map as gamer_year_map LEFT OUTER JOIN gamer_year_ranking as gamer_year_ranking on gamer_year_ranking.gamer_id = gamer_year_map.gamer_id WHERE gamer_year_map.gamer_id = gamers.gamer_id and gamer_year_map.year = ".$_GET['year']." order by gamer_number asc";
 }
 $result = mysql_query($query);
 
 $arrPoints = array();
 
 while($row = mysql_fetch_object($result)){
-	$asoPoints = array("gamer_id"=>$row->gamer_id, "lastname"=>$row->lastname, "firstname"=>$row->firstname, "nickname"=>$row->nickname, "gamer_number"=>$row->gamer_number);
+	$asoPoints = array("gamer_id"=>$row->gamer_id, "lastname"=>$row->lastname, "firstname"=>$row->firstname, "nickname"=>$row->nickname, "gamer_number"=>$row->gamer_number, "2018"=>$row->nlpt_18, "2017"=>$row->nlpt_17, "2016"=>$row->nlpt_16, "2015"=>$row->nlpt_15);
 	array_push($arrPoints,$asoPoints); 
 }
 
---
+++ WebContent/Special.php
@@ -0,0 +1,27 @@
+<?php
+
+include('db_connection.php');
+
+$year = $_GET['year'];
+$round = $_GET['round'];
+
+if($round != 0){
+  $query = "SELECT *, gamer_year_map.gamer_number FROM `points` as points left OUTER JOIN gamers as gamers on gamers.gamer_id = points.gamer_id left OUTER JOIN gamer_year_map as gamer_year_map on gamer_year_map.gamer_id = gamers.gamer_id and gamer_year_map.year = ".$year." WHERE points.category_id = 7 and points.nlpt_year = ".$year." and points.round = ".$round." order by points.points desc";  
+} else {
+  $query = "SELECT *, gamer_year_map.gamer_number FROM `points` as points left OUTER JOIN gamers as gamers on gamers.gamer_id = points.gamer_id left OUTER JOIN gamer_year_map as gamer_year_map on gamer_year_map.gamer_id = gamers.gamer_id and gamer_year_map.year = ".$year." WHERE points.category_id = 7 and points.nlpt_year = ".$year." order by points.points desc";
+}
+
+
+$result = mysql_query($query);
+
+$arrPoints = array();
+
+while($row = mysql_fetch_object($result)){
+	$asoPoints = array("point_id"=>$row->point_id, "rank"=>$row->rank, "gamer_number"=>$row->gamer_number, "nickname"=>$row->nickname, "points"=>$row->points, "category"=>$row->category_id, "round"=>$row->round);
+	array_push($arrPoints,$asoPoints); 
+}
+
+$json = json_encode($arrPoints);
+
+echo $json;
+?>
