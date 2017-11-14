e41857935ba91bd217822e80c73bc70c2a643fa6
Alexander Seibel <a.seibel90@gmx.de>
2017-11-14 13:13:43 +0100

    added admin mode

--- WebContent/wp_getUserInfo.php
+++ WebContent/wp_getUserInfo.php
@@ -5,11 +5,16 @@
     if ( is_user_logged_in() ) {
         $current_user = wp_get_current_user();
         
+        if ($current_user->ID == 1){
+            $asoUser = array("wp_userid"=>$current_user->ID, "wp_nickname"=>$current_user->display_name, "wp_logged_in"=>true, "message"=>"Eingeloggt", "admin"=>true);
+        } else {
+            $asoUser = array("wp_userid"=>$current_user->ID, "wp_nickname"=>$current_user->display_name, "wp_logged_in"=>true, "message"=>"Eingeloggt", "admin"=>false);
+        }
         $asoUser = array("wp_userid"=>$current_user->ID, "wp_nickname"=>$current_user->display_name, "wp_logged_in"=>true, "message"=>"Eingeloggt");
         array_push($arrUser,$asoUser); 
 
     } else {
-        $asoUser = array("wp_userid"=>$current_user->ID, "wp_nickname"=>$current_user->display_name, "wp_logged_in"=>false, "message"=>"Bitte einloggen!");
+        $asoUser = array("wp_userid"=>$current_user->ID, "wp_nickname"=>$current_user->display_name, "wp_logged_in"=>false, "message"=>"Bitte einloggen!", "admin"=>false);
         array_push($arrUser,$asoUser); 
     }
     
